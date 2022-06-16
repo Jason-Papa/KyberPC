3ac2a351e78b688c465dddc57ff3f8a711cc7333
Jason-Papa <ip1818@ic.ac.uk>
2022-06-16 21:47:49 +0100

    merge directories

---
+++ polar_performance.py
@@ -0,0 +1,27 @@
+import numpy as np
+from polarcodes import *
+import time
+# initialise polar code
+
+start_time = time.time()
+PC = PolarCode(256, 256)
+PC.construction_type = 'bb'
+
+# mothercode construction
+design_SNR  = 65.1
+Construct(PC, design_SNR)
+
+m = np.random.randint(2, size=PC.K)
+PC.set_message(m)
+
+Encode(PC)
+encode_time = time.time() - start_time
+
+# transmit the codeword
+AWGN(PC, design_SNR)
+# decode the received codeword
+Decode(PC)
+decode_time = time.time() - start_time - encode_time
+total_time = time.time() - start_time
+
+print(f"encoding = {encode_time}, decode = {decode_time}, total = {total_time}")
